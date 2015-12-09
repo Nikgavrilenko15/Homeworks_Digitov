6864e87000fcb9921604edfcbcbffdbb4ecc9e17
Nikgavrilenko15 <mitrand808@gmail.com>
2015-12-09 17:20:37 +0300

    homework 12

---
+++ homework_12/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+    $(".slick-arrow").empty();
+});
